b1a7f2b020c961cd8de52717263e68699a64bfda
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-02-06 21:16:15 +0530

    updated

--- app/components/home.js
+++ app/components/home.js
@@ -26,18 +26,18 @@ export default function Home() {
   return (
     <>
       <div id="par" className=" bg-red-500 p-20 flex gap-4 flex-col  h-screen">
-        <div className="nav"style={{ transform: `translateY(-${scrollPosition * 0.5}px)` }}>
+        <div className="nav"style={{ transform: `translateY(-${scrollPosition * 4}px)` }}>
           <Navabr />
         </div>
         <div className="flex gap-4  flex-row">
-          <div className="hero1"style={{ transform: `translateY(-${scrollPosition * 0.2}px)` }}>
+          <div className="hero1"style={{ transform: `translateY(-${scrollPosition * 2}px)` }}>
             <Hero1 />
           </div>
-          <div className="hero2"style={{ transform: `translateY(-${scrollPosition * 0.7}px)` }}>
+          <div className="hero2"style={{ transform: `translateY(-${scrollPosition * 3}px)` }}>
             <Hero2 />
           </div>
         </div>
-        <div className="footer" style={{ transform: `translateY(-${scrollPosition * 0.9}px)` }}>
+        <div className="footer" style={{ transform: `translateY(-${scrollPosition * 4}px)` }}>
         <Footer />
         </div>
       </div>
